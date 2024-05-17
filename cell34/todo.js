b18b891ea41d8e1bee1badaf185abfacc284d33f
tatanoi <ta.phasin@gmail.com>
2024-05-17 18:26:33 +0700

    nothing

--- cell34/todo.js
+++ cell34/todo.js
@@ -1,43 +1,43 @@
-$(document).ready(function(){
+   $(document).ready(function(){
 
-  $('#New').click(function(){
-    var name = prompt("Please enter your name:", "");
-    if (name == null || name == ''){
-      alert("invalid");
-      return 
-    }
-    setCookie(name,"1",365);  
-    console.log(name);
-    appending(name);
-  });
+      $('#New').click(function(){
+        var name = prompt("Please enter your name:", "");
+        if (name == null || name == ''){
+          alert("invalid");
+          return 
+        }
+        setCookie(name,"1",365);  
+        console.log(name);
+        appending(name);
+      });
 
-  function appending(name){
-    var todo_div = $('<div></div>')
-        .text(name)
-        .attr('id', name)
-        .attr('class',"each_todo")
-    $('#ft_list').append(todo_div);
-  }
+      function appending(name){
+        var todo_div = $('<div></div>')
+            .text(name)
+            .attr('id', name)
+            .attr('class',"each_todo")
+        $('#ft_list').append(todo_div);
+      }
 
-  function setCookie(cname, cvalue, exdays){
-    const expDate = new Date();
-    expDate.setTime(expDate.getTime() + (exdays*24*60*60*1000));
-    $.cookie(cname,cvalue,{ path: '/', expires: expDate })
-  }
+      function setCookie(cname, cvalue, exdays){
+        const expDate = new Date();
+        expDate.setTime(expDate.getTime() + (exdays*24*60*60*1000));
+        $.cookie(cname,cvalue,{ path: '/', expires: expDate })
+      }
 
-  $('#ft_list').on('click', '.each_todo', function(){
-    console.log("selected");
-    if (confirm("confirmation")){
-      deleteCookie(this.id);
-      $(this).remove();
-    }
-  });
+      $('#ft_list').on('click', '.each_todo', function(){
+        console.log("selected");
+        if (confirm("confirmation")){
+          deleteCookie(this.id);
+          $(this).remove();
+        }
+      });
 
-  function deleteCookie(name){
-    setCookie(name, null, 0);
-  }
+      function deleteCookie(name){
+        setCookie(name, null, 0);
+      }
 
-  });
+      });
   
   // function getCookie(name) {
   //   let decodedCookie = decodeURIComponent(document.cookie);
